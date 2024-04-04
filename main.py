045e44a2524c8347aa1bbe1fe2b0d9cc640cb924
Marcus Moraes <marcus.moraes@devio.com.br>
2024-04-04 13:07:36 -0300

    refactor: use saved accounts for bank operations

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import datetime
 DAILY_WITDRAWS_LIMIT = 3 
 WITDRAW_MONEY_LIMIT = 500
 
-balance = 0
-operations = []
-
 OPERATIONS = {
     'deposit': 'depósito',
     'withdraw': 'saque'
 }
 
-def deposit():
-    global balance
+def deposit(accounts):
+    account_number = input("Digite o número da conta: ")
+    account = next((a for a in accounts if a['number'] == account_number), None)
+    if not account:
+        print("Conta não encontrada!")
+        return
+
     valueToDeposit = 0
 
     try:
@@ -25,10 +27,10 @@ def deposit():
         print("Valor inválido!")
         return
     
-    balance += valueToDeposit
+    account['balance'] += valueToDeposit
 
     currentDate = datetime.datetime.now()
-    operations.append({
+    account['operations'].append({
         'type': OPERATIONS['deposit'],
         'value': valueToDeposit, 
         'date': currentDate.strftime('%d/%m/%Y'),
@@ -37,16 +39,22 @@ def deposit():
 
     print(f"Depósito de R${valueToDeposit: .2f} realizado com sucesso!")
 
-def show_statement():
+def show_statement(accounts):
+    account_number = input("Digite o número da conta: ")
+    account = next((a for a in accounts if a['number'] == account_number), None)
+    if not account:
+        print("Conta não encontrada!")
+        return
+
     print("Extrato bancário")
-    print(f"Saldo: R${balance: .2f}")
+    print(f"Saldo: R${account['balance']: .2f}")
     print("Operações realizadas:")
 
-    if(len(operations) == 0):
+    if(len(account['operations']) == 0):
         print("Nenhuma operação realizada!")
         return
     
-    for operation in operations:
+    for operation in account['operations']:
         print('-' * 30)
         print(f"""    
         Tipo: {operation['type']}
@@ -55,14 +63,17 @@ def show_statement():
         Hora: {operation['time']}
         """)
 
-def withdraw():
-    global balance
-    global operations
+def withdraw(accounts):
+    account_number = input("Digite o número da conta: ")
+    account = next((a for a in accounts if a['number'] == account_number), None)
+    if not account:
+        print("Conta não encontrada!")
+        return
 
     withdraws_today = 0
     currentDate = datetime.datetime.now().strftime('%d/%m/%Y')
    
-    for operation in operations:
+    for operation in account['operations']:
         if operation['type'] == OPERATIONS['withdraw'] and operation['date'] == currentDate:
             withdraws_today += 1
             
@@ -85,12 +96,12 @@ def withdraw():
         print(f"Valor máximo para saque é de R${WITDRAW_MONEY_LIMIT: .2f}")
         return
 
-    if valueToWithdraw > balance:
+    if valueToWithdraw > account['balance']:
         print("Saldo insuficiente!")
         return
 
-    balance -= valueToWithdraw
-    operations.append({
+    account['balance'] -= valueToWithdraw
+    account['operations'].append({
         'type': OPERATIONS['withdraw'],
         'value': valueToWithdraw,
         'date': currentDate,
@@ -152,7 +163,8 @@ def create_account(accounts, users):
         'user': None,
         'number': '',
         'agency': '0001',
-        'balance': 0
+        'balance': 0,
+        'operations': []
     }
 
     cpf = input("Digite o CPF do usuário: ")
@@ -167,7 +179,7 @@ def create_account(accounts, users):
     account['number'] = str(len(accounts) + 1).zfill(4)
     accounts.append(account)
 
-    print("Conta criada com sucesso!")
+    print(f"Conta {account['number']} criada com sucesso na agência {account['agency']}!")
 
 def list_accounts(users, accounts):
     cpf = input("Digite o CPF do usuário: ")
@@ -214,11 +226,11 @@ def main():
         option = input("Escolha uma opção: ")
 
         if option == 'd':
-            deposit()      
+            deposit(accounts=accounts)      
         elif option == 'e':
-            show_statement() 
+            show_statement(accounts=accounts) 
         elif option == 's':
-            withdraw()
+            withdraw(accounts=accounts)
         elif option == 'c':
             register_user(users)
         elif option == 'l':
